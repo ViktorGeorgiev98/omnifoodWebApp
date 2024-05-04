702c3fc5fdf30a7f50fa56f20e7a046024e8e0a3
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-05-04 15:46:57 +0300

    added smooth scrolling animation

--- src/main/js/script.js
+++ src/main/js/script.js
@@ -9,3 +9,34 @@ btnNavEl.addEventListener("click", (e) => {
 });
 
 //////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function(link) {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const href = link.getAttribute("href");
+        if (href === '#') window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+        // Scroll to other links
+        if (href !== '#' && href.startsWith('#')) {
+            const sectionEl = document.querySelector(href)
+            sectionEl.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+
+        // Close mobile navigation
+        if (link.classList.contains("main-nav-link")) {
+            headerEl.classList.toggle("nav-open");
+        }
+    })
+}) 
+
+
+////////////////////////////////////////////////
