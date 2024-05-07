a7339bff0975235c4af2729a3736d8f2c78b53de
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-05-07 16:14:04 +0300

    finished the website

--- src/main/js/script.js
+++ src/main/js/script.js
@@ -37,6 +37,27 @@ allLinks.forEach(function(link) {
         }
     })
 }) 
-
-
 ////////////////////////////////////////////////
+
+//////////////////////////////////////////////
+// Sticky navigation
+const obs = new IntersectionObserver(function(entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+        document.body.classList.add('sticky');
+    } else {
+        document.body.classList.remove('sticky');
+    }
+
+},
+ {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px"
+});
+const sectionHeroEl = document.querySelector(".section-hero");
+obs.observe(sectionHeroEl);
+
+// end of sticky navigation
+/////////////////////////////////////////////////
